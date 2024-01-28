2f4a5700258d2309580690fbb62d15f8cecf8d99
Yareeen <yaren1763@gmail.com>
2024-01-29 01:53:35 +0300

    Spring boot 3.2.1'e çekilmesi

--- src/main/java/com/book/library/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.book.library.repository;
-
-
-import com.book.library.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-    //sorgular
-}
